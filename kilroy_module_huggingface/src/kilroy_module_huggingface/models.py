7a124be810a1faf5b1e94f44513549adae79aa49
Sebastian Pietras <01133337@pw.edu.pl>
2022-08-25 00:04:09 +0200

    Refactored code into another package

--- kilroy_module_huggingface/src/kilroy_module_huggingface/models.py
+++ kilroy_module_huggingface/src/kilroy_module_huggingface/models.py
@@ -1,20 +1,22 @@
 import re
 from abc import ABC
-from typing import Optional, TypeVar
+from typing import Optional
 
 import torch
-from kilroy_module_pytorch_py_sdk import pack_padded, unpack_to_padded
+from kilroy_module_pytorch_py_sdk import (
+    LanguageModel,
+    RewardModel,
+    pack_padded,
+    unpack_to_padded,
+)
 from torch import Tensor, nn
 from torch.nn.utils.rnn import PackedSequence
 from transformers import (
+    AutoConfig,
     AutoModelForCausalLM,
     AutoModelForSequenceClassification,
-    AutoTokenizer,
     PreTrainedModel,
-    PreTrainedTokenizerBase,
 )
-
-T = TypeVar("T")
 
 
 def make_mask(x: Tensor, lengths: Tensor) -> Tensor:
@@ -23,25 +25,19 @@ def make_mask(x: Tensor, lengths: Tensor) -> Tensor:
     return indices < lengths
 
 
-class HuggingfaceModelBase(nn.Module, ABC):
+class HuggingfaceModelBase(ABC):
     def __init__(
         self,
         model: PreTrainedModel,
-        tokenizer: PreTrainedTokenizerBase,
         pad_token_id: int,
     ) -> None:
         super().__init__()
         self._model = model
-        self._tokenizer = tokenizer
         self._pad_token_id = pad_token_id
 
     @property
     def base_model(self) -> nn.Module:
         return self._model.base_model
-
-    @property
-    def tokenizer(self) -> PreTrainedTokenizerBase:
-        return self._tokenizer
 
     def freeze(self, pattern: Optional[str] = ".*") -> None:
         if pattern is not None:
@@ -52,18 +48,17 @@ class HuggingfaceModelBase(nn.Module, ABC):
                 parameter.requires_grad = False
 
 
-class HuggingfaceLanguageModel(HuggingfaceModelBase):
+class HuggingfaceLanguageModel(HuggingfaceModelBase, LanguageModel):
     @classmethod
     def from_path(cls, path: str) -> "HuggingfaceLanguageModel":
-        tokenizer = AutoTokenizer.from_pretrained(path)
         model = AutoModelForCausalLM.from_pretrained(path)
-        if tokenizer.pad_token_id is not None:
-            pad_token_id = tokenizer.pad_token_id
-        elif tokenizer.eos_token_id is not None:
-            pad_token_id = tokenizer.eos_token_id
+        if model.config.pad_token_id is not None:
+            pad_token_id = model.config.pad_token_id
+        elif model.config.eos_token_id is not None:
+            pad_token_id = model.config.eos_token_id
         else:
             pad_token_id = 0
-        return cls(model, tokenizer, pad_token_id)
+        return cls(model, pad_token_id)
 
     def freeze(self, pattern: Optional[str] = ".*") -> None:
         super().freeze(pattern)
@@ -79,14 +74,14 @@ class HuggingfaceLanguageModel(HuggingfaceModelBase):
         return pack_padded(y.logits.log_softmax(-1), lengths)
 
 
-class HuggingfaceRegressionModel(HuggingfaceModelBase):
+class HuggingfaceRegressionModel(HuggingfaceModelBase, RewardModel):
     @classmethod
     def from_path(cls, path: str) -> "HuggingfaceRegressionModel":
-        tokenizer = AutoTokenizer.from_pretrained(path)
-        if tokenizer.pad_token_id is not None:
-            pad_token_id = tokenizer.pad_token_id
-        elif tokenizer.eos_token_id is not None:
-            pad_token_id = tokenizer.eos_token_id
+        config = AutoConfig.from_pretrained(path)
+        if config.pad_token_id is not None:
+            pad_token_id = config.pad_token_id
+        elif config.eos_token_id is not None:
+            pad_token_id = config.eos_token_id
         else:
             pad_token_id = 0
         model = AutoModelForSequenceClassification.from_pretrained(
@@ -95,7 +90,7 @@ class HuggingfaceRegressionModel(HuggingfaceModelBase):
             problem_type="regression",
             pad_token_id=pad_token_id,
         )
-        return cls(model, tokenizer, pad_token_id)
+        return cls(model, pad_token_id)
 
     def freeze(self, pattern: Optional[str] = ".*") -> None:
         super().freeze(pattern)
--- kilroy_module_huggingface/src/kilroy_module_huggingface/trainers/__init__.py
+++
@@ -1,3 +0,0 @@
-from kilroy_module_huggingface.trainers.base import Trainer
-from kilroy_module_huggingface.trainers.basic import BasicTrainer
-from kilroy_module_huggingface.trainers.rewardmodel import RewardModelTrainer
